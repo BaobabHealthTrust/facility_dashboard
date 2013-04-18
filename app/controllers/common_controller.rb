80de9e5ea40f582989705221b34269f8fc902e7f
timmtonga <timmtonga@gmail.com>
2013-04-18 16:43:57 +0200

    added page and method for displaying announcements

--- app/controllers/common_controller.rb
+++ app/controllers/common_controller.rb
@@ -4,5 +4,13 @@ class CommonController < ApplicationController
   def index
     @facility = "Kamuzu Central Hospital"
   end
+	
+	def display_announcements
 
+
+    @announcements = Message.find(:all, :conditions => ["msg_type = 'announcement'
+      AND start_date <= ? and end_date >= ?",DateTime.now,DateTime.now])
+	
+	end
+	
 end
