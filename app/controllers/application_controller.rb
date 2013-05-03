0ea0047219b2cc4b22939994d5182929370a19a7
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2013-05-03 09:18:51 +0200

    Added new pages and modified slide_engine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,22 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  before_filter :set_location, :retrieve_announcements
+  before_filter :set_location
+
+  def next_path
+
+    $slide = SlideEngine.new if $slide.nil?
+
+    if !$current_slide.nil? && ($current_slide + 1 == $slide.slide_show.length)
+      $slide = SlideEngine.new
+    end
+
+    $slide.move_to_next_slide
+
+    $current_slide = $slide.current_slide
+
+    redirect_to $slide.slide_show[$current_slide]
+  end
 
   protected
 
@@ -13,31 +28,10 @@ class ApplicationController < ActionController::Base
     @facility = "Kamuzu Central Hospital"
   end
 
-  def retrieve_announcements
-    @facility = "Kamuzu Central Hospital"
-    
-    i = 0
-    @news = Message.find(:all, :select => ["msg_id, msg_id * RAND() AS "+
-          "random_no, msg_type, heading, msg_text, start_date, end_date"], :order => "random_no", :limit => 10,
-      :conditions => ["msg_type = 'announcement' AND start_date <= ? and end_date >= ?",
-        DateTime.now, DateTime.now]).collect{|n|
-      i += 1;
-      "<span style='color: #{cycle("#cfe7f5", "#dc9746", i)}'><b>#{n.heading}:</b> <i>" +
-        "#{n.msg_text[0..100]}#{(n.msg_text.length > 100 ? "..." : "")}</i></span>"
-    }.join("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ")
-
-    @news = "No Announcements" if @news.strip.blank?
-
-    @news
-
-  end
-
   def cycle(c1, c2, p)
     return c1 if p%2 > 0
     return c2
   end
-
-  protected
 
   def check_login
 
